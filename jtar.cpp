febefb91e8ab5cf00fd4b82594d34e6154ff5b3a
Ervin E. Pangilinan <Ervin.Escalona.Pangilinan@live.mercer.edu>
2023-03-03 11:51:32 -0500

    Starting Implementation

---
+++ jtar.cpp
@@ -0,0 +1,30 @@
+/*
+Ervin Pangilinan
+CSC 310: File Structures & Advanced Algorithms - Spring 2023
+Project 2: The jtar Software
+Implementation in C++
+*/
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <algorithm>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <utime.h>
+#include <fstream>
+#include "file.cpp"
+using namespace std;
+
+
+
+int main(int argc, char* argv[]) {
+    // PRE: Command-line arguments are used to specify how jtar will operate.
+    // POST: Executes based on the flags passed on command line.
+
+    
+
+
+    return 0;
+}
